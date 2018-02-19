853580c594218c8a29cde5b5f041d4be8937da8f
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-02-19 09:40:45 +0530

    Ading the breaking app

---
+++ BreakingApp/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreakingApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok("Application is running");
+        }
+
+       
+
+        // POST api/values
+        [HttpPost]
+        public void Post()
+        {
+            throw new ApplicationException("Boom!!!!! Blast of te year");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
